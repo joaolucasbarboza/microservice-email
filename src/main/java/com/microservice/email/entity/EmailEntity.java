ca1759a204604d66de6942968d1adadca61ff101
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-08-20 19:56:34 -0300

    feat: criar a entidade do banco EmailEntity

---
+++ src/main/java/com/microservice/email/entity/EmailEntity.java
@@ -0,0 +1,42 @@
+package com.microservice.email.entity;
+
+import com.microservice.email.enums.StatusEmail;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail status;
+}
