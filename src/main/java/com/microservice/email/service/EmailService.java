83d4d1cfc4c0b4221732e66500f90eb43b58724a
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-08-20 20:07:02 -0300

    feat: criar o service EmailService

---
+++ src/main/java/com/microservice/email/service/EmailService.java
@@ -0,0 +1,12 @@
+package com.microservice.email.service;
+
+import com.microservice.email.repository.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    EmailRepository emailRepository;
+}
